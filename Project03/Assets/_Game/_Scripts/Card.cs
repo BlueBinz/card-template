5fd603210ffa1c964a1bc46832d85f43164ecc31
Djeggis <Andrew.William.Gerungan@gmail.com>
2023-05-02 19:27:46 -0500

    Card object creation and organization
    Created Card Game Object and organized its attributes into a scriptable object. Card GameObject has text attributes that are updated alongside the actual attributes

---
+++ Project03/Assets/_Game/_Scripts/Card.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    [Header("Card Data")]
+    [SerializeField] private CardData _data;
+
+    //holds all of the text attributes
+    [Header("Text Attributes")]
+    [SerializeField] private TextMeshProUGUI _cardTypeText;
+    [SerializeField] private TextMeshProUGUI _cardTextText;
+    [SerializeField] private TextMeshProUGUI _cardCostText;
+    [SerializeField] private TextMeshProUGUI _cardAtkText;
+    [SerializeField] private TextMeshProUGUI _cardDefText;
+    [SerializeField] private TextMeshProUGUI _cardFlavorText;
+    [SerializeField] private TextMeshProUGUI _cardNameText;
+
+    //temporary stat counters for buffs / debuffs
+    private int tempAtk = 0;
+    private int tempDef = 0;
+
+    private int damage = 0;
+
+    //sets text attributes to script attributes
+    private void Start()
+    {
+        _cardTypeText.text = _data.Type.ToString();
+        _cardTextText.text = _data.Text;
+        _cardCostText.text = _data.Cost.ToString();
+        _cardAtkText.text = _data.Atk.ToString();
+        _cardDefText.text = _data.Def.ToString();
+        _cardFlavorText.text = _data.Flavor;
+        _cardNameText.text = _data.Name;
+    }
+}
---
+++ Project03/Assets/_Game/_Scripts/CardData.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewCardData", menuName = "CardData/Card")]
+public class CardData : ScriptableObject
+{
+    [Header("General Info")]
+    [SerializeField] private string _name;
+    [SerializeField] private CardType _type = CardType.None;
+    [Tooltip("Card text that describes its effect(s).")]
+    [SerializeField] private string _text;
+    [Tooltip("Text at the bottom of the card that adds context or lore.")]
+    [SerializeField] private string _flavor;
+
+    [Header("Stats")]
+    [SerializeField] [Range(1, 9)] private int _cost;
+    [SerializeField] private int _atk;
+    [SerializeField] private int _def;
+
+    //getters
+    public string Name => _name;
+    public CardType Type => _type;
+    public string Text => _text;
+    public string Flavor => _flavor;
+    public int Cost => _cost;
+    public int Atk => _atk;
+    public int Def => _def;
+}
